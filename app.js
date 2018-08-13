a4443adcb0cd37dedf8602aae16493c0b02d6193
Michael von Koczian <mvkoczian@Michaels-MBP-3.fritz.box>
2018-08-13 10:51:06 +0200

    Server Setup

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 
 const express 			= require('express'),
 	  sassMiddleware	= require('node-sass-middleware')
-	  path				= require('path');
+	  bodyparser		= require('body-parser'),
+	  mongoose			= require('mongoose'),
+	  passport			= require('passport'),
+	  localStrategy		= require('passport-local'),
+	  methodOverride	= require('method-override'),
+	  expressSession	= require('express-session');
 
 
 //Require Route Files
 const indexRoutes		= require('./routes/index');
+
+//Initialize Mongo DB
+
+mongoose.connect('mongodb://localhost:27017/avdb',{useNewUrlParser: true});
 
 //Init Express
 const app = express();
@@ -17,8 +26,12 @@ app.use(sassMiddleware({
 	dest: __dirname + '/public',
 	debug: true
 }))
+
 app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
 
 //Load Routes
 app.use(indexRoutes);
